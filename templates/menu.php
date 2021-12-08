bfe7e19db92058cb78f7eb39caf1468936af8007
Tom de Pasquale <tomarthus72@gmail.com>
2021-12-08 15:36:21 +0100

    Création du menu
    Closes #14

---
+++ templates/menu.php
@@ -0,0 +1,43 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top p-2">
+    <div class="container-fluid">
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+
+                <li class="nav-item">
+                    <a href="../index.php" class="nav-link">Accueil</a>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button">Clients</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="fiche_client.php">Voir fiche client</a></li>
+                        <li><a class="dropdown-item" href="liste_clients.php">Voir liste clients</a></li>
+
+                        <li><hr class="dropdown-divider"></li>
+
+                        <li><a class="dropdown-item" href="ajout_client.php">Ajouter un client</a></li>
+                        <li><a class="dropdown-item" href="editer_fiche_client.php">Éditer fiche client</a></li>
+                    </ul>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button">Commandes</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="commande.php">Voir une commande</a></li>
+                        <li><a class="dropdown-item" href="liste_commande.php">Voir la liste des commandes</a></li>
+
+                        <li><hr class="dropdown-divider"></li>
+
+                        <li><a class="dropdown-item" href="nouvelle_commande.php">Créer une commande</a></li>
+                        <li><a class="dropdown-item" href="editer_commande.php">Éditer une commande</a></li>
+                        <li><a class="dropdown-item" href="supprimer_commande.php">Supprimer une commande</a></li>
+                    </ul>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="generer_facture.php">Générer une facture</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
