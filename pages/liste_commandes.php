57b013c9d0f93b26dc6d2338afb6a8ff580cf5f2
Tom de Pasquale <tom.de_pasquale.etu@univ-lemans.fr>
2021-12-15 08:20:41 +0100

    Ajout du contenu

--- pages/liste_commandes.php
+++ pages/liste_commandes.php
@@ -7,5 +7,27 @@
 <body>
 <?php  include("../templates/menu.php");  ?>
 
+<?php
+
+require_once '../lib/Commande.php';
+$commande1 = new Commande(86534);
+$commande1->changerStatut(StatutCommande::attente_validation);
+
+$commande2 = new Commande(3654156);
+$commande2->changerStatut(StatutCommande::livree);
+
+$commandes = [$commande1, $commande2];
+?>
+
+<div class="container p-5">
+    <div class="accordion">
+        <?php
+        foreach ($commandes as $commande) {
+            $commande->afficherAppercu();
+        }
+        ?>
+    </div>
+
+</div>
 </body>
 </html>
