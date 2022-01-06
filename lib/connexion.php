16a4d1f598271821ad36da6ce50c10816b4dcfa9
guyguyy <guilhem2103bg@gmail.com>
2022-01-06 16:32:18 +0100

    j'ai modifié les commandes et la connexion les amis

---
+++ lib/connexion.php
@@ -0,0 +1,16 @@
+<?php
+//paramètres de connexion à la base de données
+$Server="localhost";
+$User="root";
+$Pwd="";
+$DB="entreprise";
+
+//connexion au serveur où se trouve la base de données
+$connexion = mysqli_connect($Server, $User, $Pwd, $DB);
+
+//affichage d’un message d’erreur si la connexion a été refusée
+if (!$connexion)
+    echo "Connexion à la base impossible";
+
+
+?>
---
+++ lib/creer_commande.php
@@ -0,0 +1,16 @@
+
+
+<?php
+
+$serveur = "localhost";
+$dbname = "cours";
+$user = "root";
+$pass = "root";
+
+$date = $_POST["date"];
+$client = $_POST["id_client"];
+$paiement = $_POST["id_paiement"];
+
+insert into commande('')
+
+?>
