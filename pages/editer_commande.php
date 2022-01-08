1419d22c8befea8dc31750514cf94367c0f5c005
matt53160 <94000752+matt53160@users.noreply.github.com>
2022-01-08 22:59:54 +0100

    Add files via upload

--- pages/editer_commande.php
+++ pages/editer_commande.php
@@ -7,57 +7,5 @@
 <body>
 <?php  include("../templates/menu.php");  ?>
 
-<?php
-require_once '../lib/Commande.php';
-require_once '../lib/Client.php';
-require_once '../lib/ArticleCommande.php';
-
-$client1 = new Client(
-    955121,
-    "De Pasquale",
-    "Tom",
-    "Silver",
-    "Tom de Pasquale",
-    "@tom_depasquale",
-    "tomdepasquale1@gmail.com");
-
-$commande1 = new Commande(86534, $client1);
-$commande1->changerStatut(StatutCommande::en_cours);
-$commande1->ajouterCommentaire("Cette commande est prise en charge par Tom de Pasquale.");
-$commande1->ajouterArticle(new ArticleCommande(652, "Caudalie Duo Levre Main", 1, 13.00));
-$commande1->ajouterArticle(new ArticleCommande(5563, "Nuxe lait corps 200ml", 2, 18.00));
-?>
-
-<div class="container p-5">
-    <div>
-        <table class="mb-3">
-            <h4>Commande <span>n°<?php echo $commande1->getId(); ?></span></h4>
-            <tr>
-                <td><?php $commande1->afficherIconeStatut(); ?></td>
-                <td><?php $commande1->afficherNomStatut(); ?></td>
-                <td><?php $commande1->afficherDerniereDate(); ?></td>
-                <td></td>
-            </tr>
-        </table>
-
-        <?php
-        $statut = $commande1->recupererStatus();
-        switch($statut) {
-            case StatutCommande::attente_validation:
-                echo "<a href='../lib/changer_statut_commande.php?id_commande=". $commande1->getId() ."&statut_actuel=". $commande1->recupererStatus() ."' class='btn btn-secondary'>Valider la commande</a>";
-                break;
-            case StatutCommande::en_cours:
-                echo "<a href='../lib/changer_statut_commande.php?id_commande=". $commande1->getId() ."&statut_actuel=". $commande1->recupererStatus() ."' class='btn btn-secondary'>Valider que la commande a été livrée</a>";
-                break;
-        }
-
-        ?>
-
-        <a href="../lib/annuler_commande.php?id_commande=<?php echo $commande1->getId(); ?>" class="btn btn-danger">Annuler la commande</a>
-
-        <?php $commande1->afficherArticlesEdition(); ?>
-    </div>
-</div>
-
 </body>
 </html>
